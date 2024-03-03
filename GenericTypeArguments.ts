50c21bce974c82e1b97d572e00f3fb24a9271e58
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-03 19:49:01 +0530

    feat: generic type arguments

---
+++ GenericTypeArguments.ts
@@ -0,0 +1,5 @@
+type GroceryStore<X extends string, Y extends string> = { name: X, city: Y};
+
+type GroceryItem = unknown;
+
+type CapreseSalad = { name: 'Caprese Salad'; price: 14.99; inStock: true };
