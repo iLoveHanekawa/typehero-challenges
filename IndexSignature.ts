5cd4d3736c4b83033cd54cc6c626b86da208b341
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-03 19:29:33 +0530

    feat: Index signature

---
+++ IndexSignature.ts
@@ -0,0 +1,7 @@
+type GroceryList = { [key: string]: number };
+
+type InappropriateActionBySituation = { [key: string]: string[] };
+
+type CharactersById = {
+	[key: string]: { id: number; name: string; status: string; species: string };
+};
