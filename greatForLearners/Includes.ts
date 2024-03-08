61240820462a04f3c8d5c12c4bb768f9872858d2
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-08 21:44:31 +0530

    feat: includes

---
+++ greatForLearners/Includes.ts
@@ -0,0 +1,9 @@
+type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2
+	? true
+	: false;
+
+type Includes<T extends readonly any[], U> = T extends [infer Item, ...infer Rest]
+	? Equal<U, Item> extends true
+		? true
+		: Includes<Rest, U>
+	: false;
